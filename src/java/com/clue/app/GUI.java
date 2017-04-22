f9ab7d15c6ea30b45339958360036cd60a3c944c
Nick Maludy <nick.maludy@encore.tech>
2017-04-22 13:30:53 -0400

    Added GUI classes

---
+++ src/java/com/clue/app/GUI.java
@@ -0,0 +1,30 @@
+package com.clue.app;
+
+import org.zeromq.ZMQ;
+import org.zeromq.ZMQ.Context;
+import org.zeromq.ZMQ.Socket;
+
+import com.clue.app.Logger;
+import com.clue.route.Message;
+import com.clue.route.NetworkMessageHandler;
+
+import com.clue.proto.Msg.*;
+import com.clue.proto.Data.*;
+
+/**
+* Hello World client
+* Connects REQ socket to tcp://localhost:5559
+* Sends "Hello" to server, expects "World" back
+*/
+public class GUI implements Runnable {
+
+  private static Logger logger = new Logger(GUI.class);
+
+  public void run() {
+    logger.debug("running GUI.");
+
+    // GUI
+    
+    logger.debug("finished GUI.");
+  }
+}
